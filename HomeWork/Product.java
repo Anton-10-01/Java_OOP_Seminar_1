bc72789fdd262e67b97ede5fe9f5a7d55a4a6b17
Anton <razjivin.anton@yandex.ru>
2023-11-04 13:06:53 +0300

    Создал пременные, добавил инкапсуляцию

--- HomeWork/Product.java
+++ HomeWork/Product.java
@@ -1,5 +1,42 @@
 package Java.Java_OOP_Seminar_1.HomeWork;
 
+import java.util.ArrayList;
+
 public class Product {
+    protected String nameProduct;
+    private Double price;
+    private Double rating;
+    public Product() {
+    }
+
+    public Product(String nameProduct, Double price, Double rating) {
+        this.nameProduct = nameProduct;
+        this.price = price;
+        this.rating = rating;
+    }
+    
+    public String getNameProduct() {
+        return nameProduct;
+    }
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public Double getRating() {
+        return rating;
+    }
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [nameProduct=" + nameProduct + ", price=" + price + ", rating=" + rating + "]";
+    }
 
 }
